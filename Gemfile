88395f3c1047cd1eaa3cf6ff4656d87404fcff23
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-02-16 23:19:31 +0000

    add rubocop

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
 gem 'rails', '~> 5.1.5'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.7'
@@ -16,6 +15,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'pundit'
+gem 'rubocop', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,31 +1,31 @@
 class ParticipationsController < ApplicationController
-	before_action :authenticate_user!
-	before_action :set_event
-	before_action :set_participation, only: :destroy
+  before_action :authenticate_user!
+  before_action :set_event
+  before_action :set_participation, only: :destroy
 
-	def create
-	    @participation = @event.participations.build
-	    @participation.user_id = current_user.id
+  def create
+  	@participation = @event.participations.build
+  	@participation.user_id = current_user.id
 
-	    if @participation.save
-	      redirect_to @event, notice: 'Participation was successfully created.'
-	    else
-	      redirect_to @event
-	    end
+  	if @participation.save
+  	  redirect_to @event, notice: 'Participation was successfully created.'
+  	else
+  	  redirect_to @event
   	end
+  end
 
-  	def destroy 
-  		@participation.destroy 
-  		redirect_to @event
-  	end
+  def destroy
+  	@participation.destroy
+  	redirect_to @event
+  end
 
-  	private
+  private
 
-  		def set_event
-  			@event = Event.find(params[:event_id])
-  		end
+  def set_event
+  	@event = Event.find(params[:event_id])
+  end
 
-  		def set_participation 
-  			@participation = @event.participations.find(params[:id])
-  		end
+  def set_participation
+  	@participation = @event.participations.find(params[:id])
+  end
 end
